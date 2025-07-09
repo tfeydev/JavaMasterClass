4f993e0dcf566fb35e5577bd433ee659780d3ebf
tfeydev <tfeydeveloper@gmail.com>
2025-07-09 14:39:46 -0300

    update

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -25,5 +25,20 @@ public class Hello {
             System.out.println("Either or both of the conditions are true");
         }
 
+        int newValue = 50;
+
+
+        if (newValue == 50) {
+            System.out.println("This is an error");
+        }
+
+        boolean isCar = false;
+
+        // if (isCar = true) {
+        // if (isCar == true) {
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
     }
 }
