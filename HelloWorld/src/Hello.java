08766072f5beff2756a3efa2b44d916b9cb2939d
tfeydev <tfeydeveloper@gmail.com>
2025-07-09 16:35:56 -0300

    update

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -40,5 +40,30 @@ public class Hello {
             System.out.println("This is not supposed to happen");
         }
 
+        String makeOfCar = "Volkswagen";
+
+        //boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+        boolean isDomestic = (!makeOfCar.equals("Volkswagen"));
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+
+        System.out.println(s);
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
     }
 }
