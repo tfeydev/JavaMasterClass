ae0fff358b3fb476f5959505379b7723feb9858d
tfeydev <tfeydeveloper@gmail.com>
2025-07-09 14:01:46 -0300

    update

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Thorsten");
+        System.out.print("Hello World");
+
+    }
+}
--- HelloWorld/src/SecondClass.java
+++ HelloWorld/src/SecondClass.java
@@ -1,4 +1,4 @@
-public class FirstClass {
+public class SecondClass {
 
     public static void main(String[] args) {
 
