52e787b829687612b7ed5b24b999c9d897a93e7b
tfeydev <tfeydeveloper@gmail.com>
2025-07-12 17:56:26 -0300

    add MainChallenge

---
+++ FirstMethodChallenge/src/main/java/MethodChallenge.java
@@ -0,0 +1,26 @@
+public class MethodChallenge {
+
+    public static void main(String[] args) {
+
+    int highScorePostion = calculateHighScorePosition(1500);
+    displayHighScorePosition("Tim", highScorePostion);
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+            + highScorePosition + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if (playerScore >= 3000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
